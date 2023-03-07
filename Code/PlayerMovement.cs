8d27f4640e0c84a49ef4acfbe98ef94da25f56d3
delarune <83880632+ctrl-alt-delarune@users.noreply.github.com>
2023-03-06 20:05:46 -0500

    Add files via upload

--- Code/PlayerMovement.cs
+++ Code/PlayerMovement.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    private float   walking_velocity        = 0;
+    private float   walking_velocity        = 0.0f;
     private Vector3 walking_velocity_vector = Vector3.zero;
     private Vector3 last_walk_forward       = Vector3.zero;
     private Vector3 last_walk_right         = Vector3.zero;
+    private float   vertical_velocity       = 0.0f;
+    private bool    jump_command            = false;
 
     private CharacterController character_controller;
 
@@ -16,6 +18,11 @@ public class PlayerMovement : MonoBehaviour
     const float DECEL_RATE =  1.0f;
     const float MAX_WSPEED =  1.0f;
     const float SPEED_MULT = 10.0f;
+
+    const float GVEL =   5.0f;
+    const float JVEL =   2.0f;
+    const float VVEL =  10.0f;
+    const float TVEL = -10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -56,8 +63,35 @@ public class PlayerMovement : MonoBehaviour
         walk_amount += walking_velocity_vector.x * last_walk_right  ;
         walk_amount += walking_velocity_vector.z * last_walk_forward;
 
+        //Jump logic
+        if (Input.GetKeyDown(KeyCode.Space))
+            jump_command = true;
+        if (Input.GetKeyUp(KeyCode.Space))
+            jump_command = false;
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position + Vector3.down, 0.5f, 0b001);
+
+        if (colliders.Length != 0)
+        {
+            
+            if (jump_command && vertical_velocity <= 0)
+                vertical_velocity = JVEL;
+
+        }
+        vertical_velocity -= Time.deltaTime * GVEL;
+
+        //Terminal velocity cap
+        if (vertical_velocity < TVEL)
+            vertical_velocity = TVEL;
+
         //Move the player by the acceleration
-        character_controller.Move(walk_amount * walking_velocity * Time.deltaTime * SPEED_MULT);
+        CollisionFlags cf = character_controller.Move(
+                 (walk_amount * walking_velocity * SPEED_MULT
+                + vertical_velocity * VVEL * Vector3.down)
+            * Time.deltaTime);
+
+        if ((cf & CollisionFlags.Below) != 0)
+            vertical_velocity = 0;
 
     }
 
